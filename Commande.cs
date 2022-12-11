75ec62cb61b3b92e5d20c5d32fe1528bc16abe23
Hp <Hp@DESKTOP-ID5FAVQ>
2022-12-11 01:27:10 +0100

    tache

--- Commande.cs
+++ Commande.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,196 @@ namespace Projet_sql_server
 {
     public partial class Commande : Form
     {
+        private int Verif;
         public Commande()
         {
             InitializeComponent();
         }
+
+        static string chaine = @"Data Source=DESKTOP-ID5FAVQ\SQLEXPRESS;Initial Catalog=VENTES;Integrated Security=True";
+
+        static SqlConnection cnx = new SqlConnection(chaine);
+        static SqlCommand cmd = new SqlCommand();
+        static SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+        public void connection()
+        {
+
+            cnx.Open();
+
+            cmd.Connection = cnx;
+        }
+
+        public void function_clear()
+        {
+            txtcmd.Clear();
+            txtdatecmd.Clear();
+
+
+        }
+
+        public void etatinitial()
+        {
+
+            function_clear();
+            txtcmd.Enabled = true;
+            txtdatecmd.Enabled = true;
+
+            btnvalider.Enabled = false;
+            btnannuler.Enabled = false;
+            btnmodifier.Enabled = true;
+            btnsupprimer.Enabled = true;
+
+            btnajouter.Enabled = true;
+
+        }
+        private void Commande_Load(object sender, EventArgs e)
+        {
+            connection();
+            btnannuler.Enabled = false;
+            btnvalider.Enabled = false;
+            btnmodifier.Enabled = true;
+            btnsupprimer.Enabled = true;
+
+            btnajouter.Enabled = true;
+            txtcmd.Enabled = false;
+            txtdatecmd.Enabled = false;
+
+            cnx.Close();
+        }
+
+        private void btnajouter_Click(object sender, EventArgs e)
+        {
+            Verif = 1;
+
+
+
+            connection();
+            function_clear();
+
+            btnannuler.Enabled = true;
+            btnmodifier.Enabled = false;
+            btnvalider.Enabled = true;
+            btnajouter.Enabled = false;
+            btnsupprimer.Enabled = false;
+
+            txtcmd.Enabled = true;
+            txtdatecmd.Enabled = true;
+
+            cnx.Close();
+
+        }
+
+        private void btnmodifier_Click(object sender, EventArgs e)
+        {
+            Verif = 2;
+
+            connection();
+            txtcmd.Enabled = true;
+            txtdatecmd.Enabled = true;
+
+            btnsupprimer.Enabled = false;
+
+            btnajouter.Enabled = false;
+            btnvalider.Enabled = true;
+            btnannuler.Enabled = true;
+            btnmodifier.Enabled = false;
+            function_clear();
+
+            cnx.Close();
+
+        }
+
+        private void btnsupprimer_Click(object sender, EventArgs e)
+        {
+            Verif = 3;
+
+            connection();
+
+
+            txtcmd.Enabled = true;
+            txtdatecmd.Enabled = false;
+
+            btnsupprimer.Enabled = false;
+
+            btnajouter.Enabled = false;
+            btnvalider.Enabled = true;
+            btnannuler.Enabled = true;
+            btnmodifier.Enabled = false;
+
+            function_clear();
+            cnx.Close();
+
+        }
+
+        private void btnafficher_Click(object sender, EventArgs e)
+        {
+            connection();
+            cmd.CommandText = "execute AffichageCommande";
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dgcmd.DataSource = dt;
+            cnx.Close();
+        }
+
+        private void btnannuler_Click(object sender, EventArgs e)
+        {
+            function_clear();
+            btnmodifier.Enabled = true;
+
+            txtcmd.Enabled = false;
+            txtdatecmd.Enabled = false;
+            btnsupprimer.Enabled = true;
+
+            btnajouter.Enabled = true;
+            btnannuler.Enabled = false;
+            btnvalider.Enabled = false;
+        }
+
+        private void btnvalider_Click(object sender, EventArgs e)
+        {
+
+            if (Verif == 1)
+            {
+                if (txtcmd.Text == "" || txtdatecmd.Text == "")
+                {
+                    MessageBox.Show("vous devez remplir les champs !!");
+                    return;
+                }
+                connection();
+
+                cmd.CommandText = " execute InsertionCommande N'" + txtcmd.Text + "',N'" + txtdatecmd.Text + "'";
+                cmd.ExecuteNonQuery();
+                etatinitial();
+
+                cnx.Close();
+
+            }
+            else if (Verif == 2)
+            {
+                connection();
+
+                cmd.CommandText = " execute Update_Commande N'" + txtcmd.Text + "',N'" + txtdatecmd.Text + "'";
+                cmd.ExecuteNonQuery();
+                etatinitial();
+                cnx.Close();
+
+            }
+            else if (Verif == 3)
+            {
+
+                MessageBox.Show("vous avez sûre !!");
+                connection();
+
+                cmd.CommandText = " execute Suppresion_Commande N'" + txtcmd.Text + "'";
+                cmd.ExecuteNonQuery();
+                etatinitial();
+                MessageBox.Show("la ligne a été bien supprimer");
+
+                cnx.Close();
+
+            }
+        }
+    
     }
 }
--- LigneCommande.Designer.cs
+++ LigneCommande.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Projet_sql_server
 {
-    partial class Commande
+    partial class LigneCommande
     {
         /// <summary>
         /// Required designer variable.
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.txtdatecmd = new System.Windows.Forms.TextBox();
-            this.txtcmd = new System.Windows.Forms.TextBox();
+            this.txtquantite = new System.Windows.Forms.TextBox();
+            this.txtlcmd = new System.Windows.Forms.TextBox();
             this.lbldatecmd = new System.Windows.Forms.Label();
-            this.lblcmd = new System.Windows.Forms.Label();
+            this.lbllcmd = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.btnannuler = new System.Windows.Forms.Button();
             this.btnvalider = new System.Windows.Forms.Button();
@@ -42,6 +42,12 @@
             this.btnajouter = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
             this.dgcmd = new System.Windows.Forms.DataGridView();
+            this.txtnumc = new System.Windows.Forms.TextBox();
+            this.txtnumcmd = new System.Windows.Forms.TextBox();
+            this.txtnump = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -50,48 +56,54 @@
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.txtdatecmd);
-            this.panel1.Controls.Add(this.txtcmd);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtnump);
+            this.panel1.Controls.Add(this.txtnumcmd);
+            this.panel1.Controls.Add(this.txtnumc);
+            this.panel1.Controls.Add(this.txtquantite);
+            this.panel1.Controls.Add(this.txtlcmd);
             this.panel1.Controls.Add(this.lbldatecmd);
-            this.panel1.Controls.Add(this.lblcmd);
-            this.panel1.Location = new System.Drawing.Point(6, 6);
+            this.panel1.Controls.Add(this.lbllcmd);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(945, 217);
-            this.panel1.TabIndex = 0;
+            this.panel1.Size = new System.Drawing.Size(945, 313);
+            this.panel1.TabIndex = 2;
             // 
-            // txtdatecmd
+            // txtquantite
             // 
-            this.txtdatecmd.Location = new System.Drawing.Point(190, 93);
-            this.txtdatecmd.Multiline = true;
-            this.txtdatecmd.Name = "txtdatecmd";
-            this.txtdatecmd.Size = new System.Drawing.Size(231, 36);
-            this.txtdatecmd.TabIndex = 3;
+            this.txtquantite.Location = new System.Drawing.Point(190, 96);
+            this.txtquantite.Multiline = true;
+            this.txtquantite.Name = "txtquantite";
+            this.txtquantite.Size = new System.Drawing.Size(231, 33);
+            this.txtquantite.TabIndex = 3;
             // 
-            // txtcmd
+            // txtlcmd
             // 
-            this.txtcmd.Location = new System.Drawing.Point(190, 34);
-            this.txtcmd.Multiline = true;
-            this.txtcmd.Name = "txtcmd";
-            this.txtcmd.Size = new System.Drawing.Size(231, 36);
-            this.txtcmd.TabIndex = 2;
+            this.txtlcmd.Location = new System.Drawing.Point(190, 40);
+            this.txtlcmd.Multiline = true;
+            this.txtlcmd.Name = "txtlcmd";
+            this.txtlcmd.Size = new System.Drawing.Size(231, 30);
+            this.txtlcmd.TabIndex = 2;
             // 
             // lbldatecmd
             // 
             this.lbldatecmd.AutoSize = true;
-            this.lbldatecmd.Location = new System.Drawing.Point(76, 103);
+            this.lbldatecmd.Location = new System.Drawing.Point(79, 113);
             this.lbldatecmd.Name = "lbldatecmd";
-            this.lbldatecmd.Size = new System.Drawing.Size(75, 16);
+            this.lbldatecmd.Size = new System.Drawing.Size(62, 16);
             this.lbldatecmd.TabIndex = 1;
-            this.lbldatecmd.Text = "Date CMD :";
+            this.lbldatecmd.Text = "Quantité :";
             // 
-            // lblcmd
+            // lbllcmd
             // 
-            this.lblcmd.AutoSize = true;
-            this.lblcmd.Location = new System.Drawing.Point(76, 54);
-            this.lblcmd.Name = "lblcmd";
-            this.lblcmd.Size = new System.Drawing.Size(94, 16);
-            this.lblcmd.TabIndex = 0;
-            this.lblcmd.Text = "Numéro CMD :";
+            this.lbllcmd.AutoSize = true;
+            this.lbllcmd.Location = new System.Drawing.Point(76, 54);
+            this.lbllcmd.Name = "lbllcmd";
+            this.lbllcmd.Size = new System.Drawing.Size(101, 16);
+            this.lbllcmd.TabIndex = 0;
+            this.lbllcmd.Text = "Numéro LCMD :";
             // 
             // panel2
             // 
@@ -101,10 +113,10 @@
             this.panel2.Controls.Add(this.btnsupprimer);
             this.panel2.Controls.Add(this.btnmodifier);
             this.panel2.Controls.Add(this.btnajouter);
-            this.panel2.Location = new System.Drawing.Point(6, 491);
+            this.panel2.Location = new System.Drawing.Point(15, 590);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(945, 97);
-            this.panel2.TabIndex = 1;
+            this.panel2.TabIndex = 3;
             // 
             // btnannuler
             // 
@@ -114,7 +126,6 @@
             this.btnannuler.TabIndex = 5;
             this.btnannuler.Text = "Annuler";
             this.btnannuler.UseVisualStyleBackColor = true;
-            this.btnannuler.Click += new System.EventHandler(this.btnannuler_Click);
             // 
             // btnvalider
             // 
@@ -124,7 +135,6 @@
             this.btnvalider.TabIndex = 4;
             this.btnvalider.Text = "Valider";
             this.btnvalider.UseVisualStyleBackColor = true;
-            this.btnvalider.Click += new System.EventHandler(this.btnvalider_Click);
             // 
             // btnafficher
             // 
@@ -134,7 +144,6 @@
             this.btnafficher.TabIndex = 3;
             this.btnafficher.Text = "Afficher";
             this.btnafficher.UseVisualStyleBackColor = true;
-            this.btnafficher.Click += new System.EventHandler(this.btnafficher_Click);
             // 
             // btnsupprimer
             // 
@@ -144,7 +153,6 @@
             this.btnsupprimer.TabIndex = 2;
             this.btnsupprimer.Text = "Supprimer";
             this.btnsupprimer.UseVisualStyleBackColor = true;
-            this.btnsupprimer.Click += new System.EventHandler(this.btnsupprimer_Click);
             // 
             // btnmodifier
             // 
@@ -154,7 +162,6 @@
             this.btnmodifier.TabIndex = 1;
             this.btnmodifier.Text = "Modifier";
             this.btnmodifier.UseVisualStyleBackColor = true;
-            this.btnmodifier.Click += new System.EventHandler(this.btnmodifier_Click);
             // 
             // btnajouter
             // 
@@ -164,37 +171,86 @@
             this.btnajouter.TabIndex = 0;
             this.btnajouter.Text = "Ajouter";
             this.btnajouter.UseVisualStyleBackColor = true;
-            this.btnajouter.Click += new System.EventHandler(this.btnajouter_Click);
             // 
             // panel3
             // 
             this.panel3.Controls.Add(this.dgcmd);
-            this.panel3.Location = new System.Drawing.Point(6, 229);
+            this.panel3.Location = new System.Drawing.Point(12, 328);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(945, 256);
-            this.panel3.TabIndex = 1;
+            this.panel3.TabIndex = 4;
             // 
             // dgcmd
             // 
             this.dgcmd.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgcmd.Location = new System.Drawing.Point(7, 5);
+            this.dgcmd.Location = new System.Drawing.Point(3, 3);
             this.dgcmd.Name = "dgcmd";
             this.dgcmd.RowHeadersWidth = 51;
             this.dgcmd.RowTemplate.Height = 24;
-            this.dgcmd.Size = new System.Drawing.Size(938, 248);
+            this.dgcmd.Size = new System.Drawing.Size(945, 245);
             this.dgcmd.TabIndex = 0;
             // 
-            // Commande
+            // txtnumc
+            // 
+            this.txtnumc.Location = new System.Drawing.Point(190, 151);
+            this.txtnumc.Multiline = true;
+            this.txtnumc.Name = "txtnumc";
+            this.txtnumc.Size = new System.Drawing.Size(231, 30);
+            this.txtnumc.TabIndex = 4;
+            // 
+            // txtnumcmd
+            // 
+            this.txtnumcmd.Location = new System.Drawing.Point(190, 203);
+            this.txtnumcmd.Multiline = true;
+            this.txtnumcmd.Name = "txtnumcmd";
+            this.txtnumcmd.Size = new System.Drawing.Size(231, 30);
+            this.txtnumcmd.TabIndex = 5;
+            // 
+            // txtnump
+            // 
+            this.txtnump.Location = new System.Drawing.Point(190, 256);
+            this.txtnump.Multiline = true;
+            this.txtnump.Name = "txtnump";
+            this.txtnump.Size = new System.Drawing.Size(231, 30);
+            this.txtnump.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Numéro client :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 217);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Numéro CMD :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 270);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Numéro produit :";
+            // 
+            // LigneCommande
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(963, 600);
-            this.Controls.Add(this.panel3);
-            this.Controls.Add(this.panel2);
+            this.ClientSize = new System.Drawing.Size(985, 688);
             this.Controls.Add(this.panel1);
-            this.Name = "Commande";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Commande_Load);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Name = "LigneCommande";
+            this.Text = "LigneCommande";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
@@ -207,10 +263,16 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox txtdatecmd;
-        private System.Windows.Forms.TextBox txtcmd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtnump;
+        private System.Windows.Forms.TextBox txtnumcmd;
+        private System.Windows.Forms.TextBox txtnumc;
+        private System.Windows.Forms.TextBox txtquantite;
+        private System.Windows.Forms.TextBox txtlcmd;
         private System.Windows.Forms.Label lbldatecmd;
-        private System.Windows.Forms.Label lblcmd;
+        private System.Windows.Forms.Label lbllcmd;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button btnannuler;
         private System.Windows.Forms.Button btnvalider;
@@ -222,4 +284,3 @@
         private System.Windows.Forms.DataGridView dgcmd;
     }
 }
-
